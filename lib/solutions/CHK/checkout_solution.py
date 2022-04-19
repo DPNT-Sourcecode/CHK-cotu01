ca57cf959908a30838821a81b73d57abe929a056
sbx_user1051 <sbx_user1051@169.254.254.253>
2022-04-19 14:56:51 +0000

    Tue Apr 19 14:56:51 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    SKU=['A','B','C','D']
+    SKU=['A','B','C','D','E']
     sku_dic={}
     skus=tuple(skus)
     for i in skus:
@@ -13,6 +13,8 @@ def checkout(skus):
             sku_dic[i]=1
         else:
             sku_dic[i]+=1
+    if 'B' in sku_dic and 'E' in sku_dic:
+        sku_dic['B']=max(0,sku_dic['B']-sku_dic['E']//2)
     value=0
     for i in sku_dic:
         if i=='C':
@@ -23,6 +25,9 @@ def checkout(skus):
             value+=sku_dic[i]%3*50+sku_dic[i]//3*130
         elif i=='B':
             value+=sku_dic[i]%2*30+sku_dic[i]//2*45
+        elif i=='E':
+            value+=sku_dic[i]*40
     return value
+
 
 
