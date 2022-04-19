c956d3e7bc467b18c4257587bb8dbb18ae1b42d5
sbx_user1051 <sbx_user1051@169.254.254.253>
2022-04-19 14:44:28 +0000

    Tue Apr 19 14:44:28 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    SKU=['A','B','C','D']
+    sku_dic={}
+    skus=tuple(skus)
+    for i in skus:
+        if i not in SKU:
+            return -1
+        if i not in sku_dic:
+            sku_dic[i]=0
+        else:
+            sku_dic[i]+=1
+    value=0
+    for i in sku_dic:
+        if i=='C':
+            value+=sku_dic[i]*20
+        elif i=='D':
+            value+=sku_dic[i]*15
+        elif i=='A':
+            value+=sku_dic[i]%3*50+sku_dic[i]//3*130
+        elif i=='B':
+            value+=sku_dic[i]%2*30+sku_dic[i]//2*45
+    return value
+
